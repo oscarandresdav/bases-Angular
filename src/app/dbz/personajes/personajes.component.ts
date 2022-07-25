3f3f8cf3366b464a2fb568514918b6b96357750b
Oscar Davila <oscar7822002@gmail.com>
2022-07-25 12:50:17 -0500

    @Input

--- src/app/dbz/personajes/personajes.component.ts
+++ src/app/dbz/personajes/personajes.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Personaje } from '../interfaces/dbz.interface';
 
 @Component({
   selector: 'app-personajes',
@@ -6,6 +7,6 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PersonajesComponent {
 
-  personajes: any[] = [];
+  @Input() personajes: Personaje[] = [];
 
 }
