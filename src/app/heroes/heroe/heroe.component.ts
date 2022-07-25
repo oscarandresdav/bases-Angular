ae5825acc4ffb3abc4faf04795b674233df4c610
Oscar Davila <oscar7822002@gmail.com>
2022-07-25 09:47:20 -0500

    Cambios en el template del componente

--- src/app/heroes/heroe/heroe.component.ts
+++ src/app/heroes/heroe/heroe.component.ts
@@ -7,4 +7,15 @@ import { Component } from "@angular/core";
 
 export class HeroeComponent {
 
+  nombre: string = 'Ironman';
+  edad: number = 45;
+
+  get nombreCapitalizado() {
+    return this.nombre.toUpperCase();
+  }
+
+  obtenerNombre(): string {
+    return `${this.nombre} - ${this.edad}`;
+  }
+
 }
