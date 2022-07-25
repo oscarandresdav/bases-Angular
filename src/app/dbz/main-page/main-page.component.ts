ea4ceafb344de9ab8bc52220286a1328ce5306b9
Oscar Davila <oscar7822002@gmail.com>
2022-07-25 14:06:15 -0500

    Tarea con inputs y módulos

--- src/app/dbz/main-page/main-page.component.ts
+++ src/app/dbz/main-page/main-page.component.ts
@@ -25,16 +25,4 @@ export class MainPageComponent {
     poder: 0
   }
 
-  agregar() {
-    if (this.nuevo.nombre.trim().length === 0) {
-      return;
-    }
-    console.log(this.nuevo);
-    this.personajes.push(this.nuevo);
-    this.nuevo = {
-      nombre: '',
-      poder: 0
-    }
-  }
-
 }
