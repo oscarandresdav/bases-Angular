13fb9be669fa300e21f9278f6258259537e2162e
Oscar Davila <oscar7822002@gmail.com>
2022-07-25 12:14:02 -0500

    ngModel

--- src/app/dbz/main-page/main-page.component.ts
+++ src/app/dbz/main-page/main-page.component.ts
@@ -1,13 +1,23 @@
 import { Component, OnInit } from '@angular/core';
 
+interface Personaje {
+  nombre: string,
+  poder: number
+}
+
 @Component({
   selector: 'app-main-page',
   templateUrl: './main-page.component.html'
 })
 export class MainPageComponent {
 
+  nuevo: Personaje = {
+    nombre: 'Trunks',
+    poder: 14000
+  }
+
   agregar() {
-    console.log('Esta es una prueba...')
+    console.log(this.nuevo)
   }
 
 }
